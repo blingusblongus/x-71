a47f654c59b0f28944076ac83dab191679631674
Nick Allen <rutherford.t.b@gmail.com>
2024-05-09 10:26:47 -0500

    add hello world get route

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -6,6 +6,10 @@ dotenv.config();
 const PORT = 3000;
 
 const app = express();
+
+app.get("/", (_req: Request, res: Response) => {
+    res.send("hello world");
+})
 
 app.listen(PORT, () => console.log("App running on port:", PORT))
 
