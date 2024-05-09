6c7ae2fbb51d1247692cdd82eb165207f3545cf9
Nick Allen <rutherford.t.b@gmail.com>
2024-05-09 12:24:45 -0500

    add simple query validation and call to nasa API

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,29 @@
-import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const PORT = 3000;
+import express, { Request, Response } from "express";
+import nasaService from "./services/nasaService";
 
+const PORT = 3000;
 const app = express();
 
-app.get("/", (_req: Request, res: Response) => {
-    res.send("hello world");
+app.get("/", async (req: Request, res: Response) => {
+    const { start_date, end_date, distance } = req.query;
+
+    if (!start_date || !end_date || !distance) {
+        return res.status(400).json({ error: true, message: "Required parameters missing" });
+    }
+    if (typeof start_date !== "string" || typeof end_date !== "string" || typeof distance !== "string") {
+        return res.status(400).json({ error: true, message: "Required parameters missing" });
+    }
+
+    try {
+        const nasaRes = await nasaService.feed(start_date, end_date)
+        res.json(nasaRes);
+    } catch (err) {
+        res.status(500).json({ error: true, message: "Error retrieving data" })
+    }
 })
 
 app.listen(PORT, () => console.log("App running on port:", PORT))
-
 
